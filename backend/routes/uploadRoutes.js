2f03f5076944c435d03e2f33a67e9d271d03d7fe
Likhitha <likithasiddu25@gmail.com>
2026-05-12 16:32:56 +0530

    Final upload route fix

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -11,19 +11,21 @@ const storage = multer.diskStorage({
     const dir = "uploads/";
 
     if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
+      fs.mkdirSync(dir, { recursive: true });
     }
 
     cb(null, dir);
   },
 
-  filename: function(req, file, cb) {
+  filename: function (req, file, cb) {
+
     cb(null, Date.now() + "-" + file.originalname);
+
   }
 
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
 router.post("/", upload.single("image"), (req, res) => {
 
