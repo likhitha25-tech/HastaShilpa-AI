869625c9f8330079652595c95cde20b1ddbe9f27
Likhitha <likithasiddu25@gmail.com>
2026-05-15 00:16:16 +0530

    Fixed auth middleware order

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
 const aiRoutes = require("./routes/aiRoutes");
 
